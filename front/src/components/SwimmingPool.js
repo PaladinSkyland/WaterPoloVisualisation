10d8d7cda7a7af1d981c0b6a13fe70660e388f73
tonelomb <lucas.brancolini@efrei.net>
2024-05-03 15:38:19 +0200

    Initial cleanup

---
+++ front/src/components/SwimmingPool.js
@@ -0,0 +1,20 @@
+import '../css/SwimmingPool.css'
+
+function SwimmingPool(){
+    return(
+        <div>
+            <div class="pool-container">
+                <div class="title">
+                    Visualisation des trajectoires de poloïstes
+                </div>
+                <div class="swimming-pool">
+                    <div class="waterpolo-pool">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SwimmingPool

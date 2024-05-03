9914362cd2b7ec12c52dddbbeda78904f5fd85d8
tonelomb <lucas.brancolini@efrei.net>
2024-05-03 17:51:40 +0200

    Main app : added the components created before

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,19 @@
 import './css/App.css';
 import './components/SwimmingPool'
 import SwimmingPool from './components/SwimmingPool';
+import Settings from './components/Settings';
+import { useState } from 'react';
 
 function App() {
+
+  const [gender, setGender] = useState("masculin");
+
+
   return (
     <div className="App">
-      <SwimmingPool/>
-
+      <SwimmingPool gender={gender}/>
+      <Settings gender={gender} setGender={setGender}/>
+      
     </div>
   );
 }

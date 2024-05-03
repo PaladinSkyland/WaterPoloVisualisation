3c208bc7b38ffadf3ed676fcde11a3f5fc797337
OUGO <hugo.parmentier@efrei.net>
2024-05-03 16:25:58 +0200

    premier affichage des coordonnée

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,29 @@
 import './css/App.css';
 import './components/SwimmingPool'
 import SwimmingPool from './components/SwimmingPool';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080'); // Adresse du serveur WebSocket
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
   return (
     <div className="App">
+      <div className="App">
+      <h1>Coordonnées X et Y</h1>
+      <p>X: {coordinates.x}, Y: {coordinates.y}</p>
+    </div>
       <SwimmingPool/>
 
     </div>

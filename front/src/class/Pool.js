ab3d81a5524be4743fe837a03131c01f4c9481f3
OUGO <hugo.parmentier@efrei.net>
2024-05-17 14:59:12 +0200

    calcul avec l'origine du repère, pas de calcul du terrain, mais avec le bassin uniquement

--- front/src/class/Pool.js
+++ front/src/class/Pool.js
@@ -1,10 +1,22 @@
 import Player from './Player';
 
-class Pool {
+class WaterPoloField {
     constructor() {
         this.sex = 'male';
         this.width = (this.sex === 'male') ? 30 : 25; // Largeur du bassin en mètres
         this.height = 20;
+    }
+}
+
+class Pool {
+    constructor() {
+        
+        this.width = 50; // Largeur du bassin en mètres
+        this.height = 25; // Hauteur du bassin en mètres
+        this.depth = 2; // Profondeur du bassin en mètres
+
+        this.origine_coord_x = 25;
+        this.origine_coord_y = -0.25;
 
         this.players = [];
     }
@@ -18,6 +30,8 @@ class Pool {
     }
 
     movePlayerOrAdd(tag, x, y, z, precision) {
+        x += this.origine_coord_x;
+        y += this.origine_coord_y;
         let player = this.players.find(player => player.tag === tag);
         if (!player) {
             let newplayer = new Player(tag, x, y, z, precision);
--- front/src/components/SwimmingPool.js
+++ front/src/components/SwimmingPool.js
@@ -9,22 +9,22 @@ function SwimmingPool(props){
                     Visualisation des trajectoires de poloïstes
                 </div>
                 
-          <div className="swimming-pool" >
-                    <div className={'waterpolo-pool ' + props.gender} style={{ position: 'relative' }}>
+          <div className="swimming-pool" style={{ position: 'relative' }}>
+                    <div className={'waterpolo-pool ' + props.gender} >
+                    </div>
                     {/* Afficher les joueurs */}
                     {props.pool.players.map((player, index) => (
               <div
                 key={index}
                 className="player"
                 style={{
-                  left: (player.y / props.pool.width) * 100 + '%',
-                  top: (player.x / props.pool.height) * 100 + '%',
+                  left: (player.x / props.pool.width) * 100 + '%',
+                  top: (player.y / props.pool.height) * 100 + '%',
                   transform: 'translate(-50%, -50%)', // Pour centrer le joueur
                   position: 'absolute', // Position absolue par rapport à la piscine parente
                 }}
               ></div>
             ))}
-                    </div>
                 </div>
             </div>
         </div>

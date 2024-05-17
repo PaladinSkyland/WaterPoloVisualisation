3ce08c959fd13b0e7d961e96f7ccb7e6f7a3e79f
Yohan Villiers <yohan.villiers@thekeepers.io>
2024-05-17 14:26:30 +0200

    use react-konva for displaying pool and players

--- front/src/components/SwimmingPool.js
+++ front/src/components/SwimmingPool.js
@@ -1,30 +1,100 @@
-import { useEffect } from 'react';
 import '../css/SwimmingPool.css'
+import { Stage, Layer, Rect, Circle, Star, Text } from 'react-konva';
+import React, { useEffect, useState, useRef } from 'react';
 
 function SwimmingPool(props){
+    const stageRef = useRef(null);
+    const containerRef = useRef(null);
+
+    const getPoolHeight = () => {
+        return 25;
+    }
+
+    const getPoolWidth = () => {
+        return 50;
+    }
+
+    const getWaterpoloPoolHeight = () => {
+        return 20;
+    }
+
+    const getWaterpoloPoolWidth = () => {
+        return props.gender === "male" ? 30 : 25;
+    }
+
+    const [dimensions, setDimensions] = useState({
+        width: getPoolWidth(),
+        height: getPoolHeight(),
+        scale: 1,
+    });
+    
+    const fitStageIntoParentContainer = () => {
+        if (containerRef.current) {
+            const containerWidth = containerRef.current.offsetWidth;
+            const scale = containerWidth / (getPoolWidth());
+            setDimensions({
+                width: getPoolWidth() * scale,
+                height: getPoolHeight() * scale,
+                scale,
+            });
+        }
+    };
+    
+    useEffect(() => {
+        fitStageIntoParentContainer();
+        window.addEventListener('resize', fitStageIntoParentContainer);
+    
+        return () => {window.removeEventListener('resize', fitStageIntoParentContainer)};
+    }, []);
+    
     return(
         <div>
             <div className="pool-container">
                 <div className="title">
                     Visualisation des trajectoires de poloïstes
                 </div>
-                
-          <div className="swimming-pool" >
-                    <div className={'waterpolo-pool ' + props.gender} style={{ position: 'relative' }}>
-                    {/* Afficher les joueurs */}
-                    {props.pool.players.map((player, index) => (
-              <div
-                key={index}
-                className="player"
-                style={{
-                  left: (player.y / props.pool.width) * 100 + '%',
-                  top: (player.x / props.pool.height) * 100 + '%',
-                  transform: 'translate(-50%, -50%)', // Pour centrer le joueur
-                  position: 'absolute', // Position absolue par rapport à la piscine parente
-                }}
-              ></div>
-            ))}
-                    </div>
+                <div ref={containerRef} className="stage-container">
+                    <Stage
+                        width={dimensions.width}
+                        height={dimensions.height}
+                        scaleX={dimensions.scale}
+                        scaleY={dimensions.scale}
+                        ref={stageRef}
+                    >
+                        <Layer className="swimming-pool">
+                            <Rect
+                                x={0}
+                                y={0}
+                                width={getPoolWidth()}
+                                height={getPoolHeight()}
+                                fill="#6472ef"
+                                stroke="black"
+                                strokeWidth={0.2}
+                            />
+                        </Layer>
+                        <Layer className="waterpolo-pool">
+                            <Rect
+                                x={2}
+                                y={2}
+                                width={getWaterpoloPoolWidth()}
+                                height={getWaterpoloPoolHeight()}
+                                border={10}
+                                stroke="black"
+                                strokeWidth={0.1}
+                            />
+                        </Layer>
+                        <Layer className="players">
+                            {props.pool.players.map((player, index) => (
+                                <Circle
+                                    key={index}
+                                    x={player.y}
+                                    y={player.x}
+                                    radius={0.5}
+                                    fill="red"
+                                />
+                            ))}
+                        </Layer>
+                    </Stage>
                 </div>
             </div>
         </div>

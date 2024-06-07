c66b797ac0a87bea5a49a0934bfa419e0bd23c6b
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-06-07 18:48:44 +0200

    Rewind work but with arthefact when display players

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,8 @@ import Pool from './class/Pool';
 import React, { useEffect, useState } from 'react';
 
 function App() {
+  const address = 'ws://localhost:8080?file=dynamic5';
+
   const [pool, setPool] = useState(new Pool());
 
   const [showInputs, setShowInputs] = useState(false);
@@ -16,12 +18,21 @@ function App() {
     ancreH: '',
     gender: 'masculin'
   });
+  
+  const [progress, setProgress] = useState(0);
+  const [[mintime,maxtime], setMinMaxTime] = useState([0,100]);
+  //const [data, setData] = useState(null);
+  const [ws, setWs] = useState(null);
 
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8080?file=dynamic5'); // Adresse du serveur WebSocket
+    const newWs = new WebSocket(address); // Adresse du serveur WebSocket
+    setWs(newWs);
 
-    ws.onmessage = (event) => {
+    newWs.onmessage = (event) => {
       const data = JSON.parse(event.data);
+      if (data.TimeData) {
+        setMinMaxTime([data.TimeData.firstTime / 1000, data.TimeData.lastTime / 1000]);
+      }
       if (data.x && data.y) {
         const { anchor, x, y, z, precision } = data;
         setPool((currentPool) => {
@@ -36,14 +47,40 @@ function App() {
     };
   
     return () => {
-      ws.close();
+      newWs.close();
     };
   }, []);
+
+    useEffect(() => {
+    const interval = setInterval(() => {
+      if (progress < maxtime) {
+        setProgress(progress + 1);
+        console.log(progress);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [progress, maxtime, ws]);
+  
+
+  const handleChangeProgress = (event) => {
+    ws.send(JSON.stringify({ timestamp: parseFloat(event.target.value)}));
+    setProgress(parseInt(event.target.value));
+  };
 
   return (
     <div className="App">
       <SwimmingPool gender={values.gender} pool={pool}/>
       <SettingsContainer values={values} setValues={setValues} showInputs={showInputs} setShowInputs={setShowInputs} />
+      <input
+        type="range"
+        min={mintime}
+        max={maxtime}
+        value={progress}
+        onChange={handleChangeProgress}
+      />
     </div>
   );
 }
